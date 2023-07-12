4d59fffbfd0db025e90451d901d09d3ba30078c8
yongjia <a592070@gmail.com>
2023-07-12 11:46:04 +0800

    easy: 268. Missing Number

---
+++ 268_Missing_Number/main_test.go
@@ -0,0 +1,53 @@
+package main
+
+import "testing"
+
+func Test_missingNumber(t *testing.T) {
+	type args struct {
+		nums []int
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		// TODO: Add test cases.
+		{"[3,0,1]", args{nums: []int{3, 0, 1}}, 2},
+		{"[0,1]", args{nums: []int{0, 1}}, 2},
+		{"[9,6,4,2,3,5,7,0,1]", args{nums: []int{9, 6, 4, 2, 3, 5, 7, 0, 1}}, 8},
+		{"[0,1,2,3,4,5,6,8,9]", args{nums: []int{0, 1, 2, 3, 4, 5, 6, 8, 9}}, 7},
+		{"[1,2,3,4,5,6,8,9,0]", args{nums: []int{0, 1, 2, 3, 4, 5, 6, 8, 9}}, 7},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := missingNumber(tt.args.nums); got != tt.want {
+				t.Errorf("missingNumber() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func Test_missingNumber2(t *testing.T) {
+	type args struct {
+		nums []int
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		// TODO: Add test cases.
+		{"[3,0,1]", args{nums: []int{3, 0, 1}}, 2},
+		{"[0,1]", args{nums: []int{0, 1}}, 2},
+		{"[9,6,4,2,3,5,7,0,1]", args{nums: []int{9, 6, 4, 2, 3, 5, 7, 0, 1}}, 8},
+		{"[0,1,2,3,4,5,6,8,9]", args{nums: []int{0, 1, 2, 3, 4, 5, 6, 8, 9}}, 7},
+		{"[1,2,3,4,5,6,8,9,0]", args{nums: []int{0, 1, 2, 3, 4, 5, 6, 8, 9}}, 7},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := missingNumber2(tt.args.nums); got != tt.want {
+				t.Errorf("missingNumber2() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
